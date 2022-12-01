956e6b8159dd1d78c5872dce755b3bf29975e918
DESKTOP-LMUOEEL\raona <raonaram77@gmail.com>
2022-12-02 00:54:23 +0900

    lib test

--- lib/pandas_exploreData.py
+++ lib/pandas_exploreData.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 def exploreData(df) :
-    print('df shape : \n', df.shape) 
-    print('df.head(3) : \n ', df.head(3))
-    print('df.info() : \n ', df.info())
-    print('df.describe() : \n', df.describe())
+    print('df shape : \n')
+    df.shape
+    print('\n df.head(3) : \n ', df.head(3))
+    print('\n df.info() : \n ', df.info())
+    print('\n df.describe() : \n', df.describe())
