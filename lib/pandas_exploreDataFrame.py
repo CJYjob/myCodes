7433418958beecf204293ad7f5fd65f2e802a0b5
DESKTOP-LMUOEEL\raona <raonaram77@gmail.com>
2022-12-02 01:41:23 +0900

    lib test

--- lib/pandas_exploreDataFrame.py
+++ lib/pandas_exploreDataFrame.py
@@ -1,14 +1,4 @@
 import pandas as pd
 
 def exploreDataFrame(df) :
-
-    print(df.shape, '\n')
-
-    print(df.head(3), '\n')
-    print('\n')
-
-    print(df.info(), '\n')
-    print('\n')
-
-    print(df.describe(), '\n')
-    print('\n')
+    print(df.shape)
