a9c3bc3654748654f7edeb64c134f06960b49018
DESKTOP-LMUOEEL\raona <raonaram77@gmail.com>
2022-12-02 19:23:05 +0900

    progressing tune ANN model

--- lib/func_debug_log.py
+++ lib/func_debug_log.py
@@ -11,8 +11,8 @@ dt_now_str = dt_now.strftime('%Y.%m.%d %H:%M:%S')
     처리
         현재 시간, 호출 파일, 로그 내용 출력
         용법 : if debug_flag in range(low, high+1) : debug(f'log', __name__)
-            자주 출력되는 놈일수록 낮은 번호(최소 low, 최대 high)에 두고,
-            high를 낮춰가며, 리스트에서 제거해서 출력 제어
+            중요한 log에 넓은 범위(최소 low, 최대 high)를 할당하고,
+            low를 높여가며, 덜중요한 로그를 제거해서 출력 제어
             Ex) # debug_log = 0 : 미출력, 1 : 함수 실행 로그 출력, 2 : 함수 내부 로그 출력
     반환
         없음
