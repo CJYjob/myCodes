c109c8fc22f3fc1cd76e9f2bd5ec401826b9db29
DESKTOP-LMUOEEL\raona <raonaram77@gmail.com>
2022-12-02 09:22:06 +0900

    progressing NN down syndrome

--- lib/pandas_readDataFrame.py
+++ lib/pandas_readDataFrame.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 # csv 파일 읽어오기
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+
+# excel 파일 읽어오기
+df2 = pd.read_excel('https://ds-lecture-data.s3.ap-northeast-2.amazonaws.com/MouseProtein/mouse_protein_X.xls', header=None)
+df2_label = pd.read_excel('https://ds-lecture-data.s3.ap-northeast-2.amazonaws.com/MouseProtein/mouse_protein_label.xls', header=None)
